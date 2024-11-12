de5c89c0321ec504a7e1b436357ed6bb958637b5
DanishManzoorUmrani <umranipak1@gmail.com>
2024-11-12 18:43:50 +0500

    rstopwatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,52 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import "./App.css"
+import gsap from 'gsap'
 
-import './App.css'
-function App() {
+const Stopwatch = () => {
+  const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState(null);
+
+  const start = () => {
+    if (!timer) setTimer(setInterval(() => setTime(t => t + 1), 1000));
+  };
+
+  const stop = () => {
+    clearInterval(timer);
+    setTimer(null);
+  };
+
+  const reset = () => {
+    clearInterval(timer);
+    setTime(0);
+    setTimer(null);
+  };
+
+  useEffect(()=>{
+    gsap.to(".watchtime",{
+      duration:2,
+      delay:1,
+      rotate:360,
+    },{scope:".watchtime"})
+  
+  })
 
 
-
-    const [time, settime] = useState(null);
-    const [now, setnow] = useState(null);
-    const interref = useRef(null);
-
-    const handlestart = () => {
-        settime(Date.now());
-        setnow(Date.now());
-        interref.current = setInterval(() => {
-            settime(Date.now());
-
-        }, 10);
-    }
-    const handlestop = () => {
-        clearInterval(interref.current)
-    };
-
-    let timepass = (time - now) / 1000;
-    return (
-        <div className='App'>
-            <h1 >Stop watch</h1>
-            <h2 className='color'> {timepass.toFixed(3)}</h2>
-            <div>
-                <button onClick={handlestart}>start</button>
-                <button onClick={handlestop}>stop</button>
-            </div>
+  
+  return (
+    <>
+      <div className='watch'>
+        <div className='watchtime'>
+          <h1>Stopwatch</h1>
+          <h2>{`${String(Math.floor(time / 60)).padStart(2, '0')}:${String(time % 60).padStart(2, '0')}`}</h2>
+          <div className="btn">
+            <button onClick={start}>Start</button>
+            <button onClick={stop}>Stop</button>
+            <button onClick={reset}>Reset</button>
+          </div>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default App
+export default Stopwatch;
